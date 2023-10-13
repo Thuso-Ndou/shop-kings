fe8b7b4048341f15968387bba6169356b7bb9fdf
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2023-10-14 01:46:43 +0200

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import './App.css';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Checkout from './Checkout';
 import Header from './Header';
+import './App.css';
 
 function App() {
-  return (<>
-  <Header/>
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/checkout' element={<Checkout/>}/>
-  </Routes>
-  </>
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <>
+            <Header />
+            <Home />
+          </>
+        }
+      />
+      <Route path='/login' element={<Login />} />
+      <Route
+        path='/checkout'
+        element={
+          <>
+            <Header />
+            <Checkout />
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
 export default App;
+
