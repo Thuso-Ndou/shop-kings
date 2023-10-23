d206fef8141291d82d2c612c6855efc98a326125
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2023-10-23 16:32:47 +0200

    axios

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // THE API (cloud function) URL
   baseURL: 'https://us-central1-shopkings-store.cloudfunctions.net/api'
+  //'http://127.0.0.1:5001/shop-kings/us-central1/api'
 });
 
 export default instance;
